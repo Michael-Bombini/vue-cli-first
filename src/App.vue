6101edc31309444772393ef8834b3412521340b1
Michael Bombini <bombini.michael@gmail.com>
2022-06-20 14:59:51 +0200

    Completed Jumbotron component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
     <TitleComponent></TitleComponent>
+    <HeroComponent></HeroComponent>
   </div>
 </template>
 
 <script>
 import TitleComponent from "./components/TitleComponent.vue";
+import HeroComponent from "./components/HeroComponent.vue";
 
 
 export default {
   name: 'App',
   components: {
-    TitleComponent
+    TitleComponent,
+    HeroComponent
 }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin : 0 ;
+  padding: 0;
+  box-sizing: border-box;
 }
+
 </style>
---
+++ src/components/HeroComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <img src="../assets/jumbotron.jpeg" alt="">
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+    img {
+        height: 600px;
+    }
+</style>
